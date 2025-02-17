8c3211dc451d1d56e2a282ca77330ce04f295b9f
vikramraajak <vikramraajak@gmail.com>
2025-02-17 21:47:39 +0530

    deployed correct

--- fitness-ui/src/App.js
+++ fitness-ui/src/App.js
@@ -41,7 +41,7 @@ function App() {
         e.preventDefault();
         setLoading(true);
         try {
-            const response = await axios.post("https://ai-personal-workout-recommender.onrender.com/", formData);
+            const response = await axios.post("https://ai-personal-workout-recommender.onrender.com/predict", formData);
             setResult(response.data);
         } catch (error) {
             console.error("Error:", error);
